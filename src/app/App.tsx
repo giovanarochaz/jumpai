887c443756d22022962e4f23bfadbe5f3a4e46ac
Giovana Rocha <giovanarocha109@gmail.com>
2025-05-09 19:59:45 -0300

    Tela - Home

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,16 +2,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import DesafioSistemaSolar from '../features/jogos/sistemaSolar/desafio/DesafioSistemaSolar';
+import Home from '../pages/home/Home';
 
 const App: React.FC = () => {
   return (
     <Router>
       <Routes>
-        {/* Rota para o jogo do sistema solar */}
+        <Route path="/" element={<Home />} />
         <Route path="/jogo/sistemaSolar/desafio" element={<DesafioSistemaSolar />} />
-
-        {/* Outras rotas, se houver */}
-        <Route path="/" element={<div>Home</div>} />
       </Routes>
     </Router>
   );
---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import './Home.css';
+import { motion } from 'framer-motion';
+
+import luva from '../../images/home/1.png';
+import olho from '../../images/home/2.png';
+import teclado from '../../images/home/3.png';
+
+const Home: React.FC = () => {
+  const opcoes = [
+    { id: 1, nome: 'Luva', imagem: luva },
+    { id: 2, nome: 'Olho', imagem: olho },
+    { id: 3, nome: 'Teclado', imagem: teclado },
+  ];
+
+  return (
+    <motion.div
+      className="container-inicio"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 2 }}
+    >
+      <h1 className="titulo-inicio">JumpAI – Onde a acessibilidade encontra a diversão</h1>
+      <p className="descricao-inicio">
+        Escolha um tipo de controle para começar a jogar. Você pode usar a luva, os movimentos dos olhos ou o teclado para se divertir com o JumpAI. A acessibilidade é nossa prioridade, então selecione o controle que melhor se adapta a você!
+      </p>
+
+      <div className="caixa-botoes">
+        {opcoes.map(({ id, nome, imagem }) => (
+          <motion.div
+            key={id}
+            className="botao-interativo"
+            whileHover={{ scale: 1.2}}
+            whileTap={{ scale: 1.4}}
+          >
+            <img
+              src={imagem}
+              alt={`Ícone de ${nome}`}
+              className="imagem-botao"
+            />
+            <p className="texto-botao">{nome}</p>
+          </motion.div>
+        ))}
+      </div>
+    </motion.div>
+  );
+};
+
+export default Home;
